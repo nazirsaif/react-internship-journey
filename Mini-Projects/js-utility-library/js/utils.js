143746c2a4ab7d62c1c0ad9026f6332ac22f0a16
saifullah nazir <saifullahnazir2020@gmail.com>
2026-07-23 15:39:30 +0500

    Upgraded throttle utility with leading/trailing edge options

--- Mini-Projects/js-utility-library/js/utils.js
+++ Mini-Projects/js-utility-library/js/utils.js
@@ -1,39 +1,67 @@
 // Debounce Utility (Pro)
 function debounce(fn, delay, immediate = false) {
     let timeoutId;
-    
     const debounced = function (...args) {
         const callNow = immediate && !timeoutId;
-        
         clearTimeout(timeoutId);
         timeoutId = setTimeout(() => {
             timeoutId = null;
             if (!immediate) fn.apply(this, args);
         }, delay);
-        
         if (callNow) fn.apply(this, args);
     };
-    
     debounced.cancel = function() {
         clearTimeout(timeoutId);
         timeoutId = null;
     };
-    
     return debounced;
 }
 
-// Throttle Utility
-function throttle(fn, limit) {
-    let inThrottle;
-    return function (...args) {
-        if (!inThrottle) {
-            fn.apply(this, args);
-            inThrottle = true;
-            setTimeout(() => {
-                inThrottle = false;
-            }, limit);
+// Throttle Utility (Pro)
+function throttle(fn, limit, options = { leading: true, trailing: true }) {
+    let timeoutId = null;
+    let lastArgs = null;
+    let lastThis = null;
+    let lastCallTime = 0;
+
+    const invokeFunc = function(time) {
+        const args = lastArgs;
+        const thisArg = lastThis;
+        lastArgs = lastThis = null;
+        lastCallTime = time;
+        fn.apply(thisArg, args);
+    };
+
+    const throttled = function(...args) {
+        const time = Date.now();
+        if (!lastCallTime && options.leading === false) lastCallTime = time;
+        
+        const remaining = limit - (time - lastCallTime);
+        lastArgs = args;
+        lastThis = this;
+
+        if (remaining <= 0 || remaining > limit) {
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+                timeoutId = null;
+            }
+            invokeFunc(time);
+        } else if (!timeoutId && options.trailing !== false) {
+            timeoutId = setTimeout(() => {
+                lastCallTime = options.leading === false ? 0 : Date.now();
+                timeoutId = null;
+                invokeFunc(Date.now());
+            }, remaining);
         }
     };
+    
+    throttled.cancel = function() {
+        clearTimeout(timeoutId);
+        lastCallTime = 0;
+        timeoutId = lastArgs = lastThis = null;
+    };
+
+    return throttled;
 }
 
 // Pub-Sub Event Bus
