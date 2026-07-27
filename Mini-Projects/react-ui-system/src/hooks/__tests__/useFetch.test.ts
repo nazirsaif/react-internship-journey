7c30f195c538b957cdc4fbc6746b6e6bcde0e03b
saifullah nazir <saifullahnazir2020@gmail.com>
2026-07-27 15:53:01 +0500

    Remove unused act import from useFetch test

--- Mini-Projects/react-ui-system/src/hooks/__tests__/useFetch.test.ts
+++ Mini-Projects/react-ui-system/src/hooks/__tests__/useFetch.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act, waitFor } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
 import { useFetch } from '../useFetch';
 import { vi } from 'vitest';
 
