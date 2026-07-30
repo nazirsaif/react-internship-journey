03f079d8f38f07a4199012c03fea86d87e554e3d
saifullah nazir <saifullahnazir2020@gmail.com>
2026-07-30 19:51:17 +0500

    Scaffolded advanced-forms consuming react-ui-system

---
+++ Mini-Projects/advanced-forms/src/App.tsx
@@ -0,0 +1,12 @@
+import { Wizard } from './components/Wizard';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Wizard />
+    </div>
+  );
+}
+
+export default App;
